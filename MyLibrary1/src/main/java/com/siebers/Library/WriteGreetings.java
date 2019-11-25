42fb94ef8d289d13957f005c44945e96b6cd74ed
Alex <1014104338@qq.com>
2019-11-25 17:44:13 +0800

    update whole file

--- MyLibrary1/src/main/java/com/siebers/Library/WriteGreetings.java
+++ MyLibrary1/src/main/java/com/siebers/Library/WriteGreetings.java
@@ -2,7 +2,7 @@ package com.siebers.Library;
 
 public class WriteGreetings {
 	
-		public static void wrtie() {
+		public static void write() {
 			System.out.println("Mave says: Nearly Christmas :)");
 		}
 
